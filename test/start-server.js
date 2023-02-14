ffca7aedabb9ff819ddb32bd4dc176ed317ea1c9
liucy427 <liucy427@163.com>
2023-02-14 12:51:49 +0800

    use cpuid to encryption

--- test/start-server.js
+++ test/start-server.js
@@ -20,17 +20,29 @@ let ivTemp2 = key2 + keye + keya;
 keyTemp1 += keyBI + keyd;
 const key4 = String.fromCharCode(key2.charCodeAt() + 2);
 const keys = 's';
+let keyDevices = keyd + keye;
 const space = String.fromCharCode(point.charCodeAt() - 14);
 const keyj = String.fromCharCode(keyd.charCodeAt() + 6);
 const slash = String.fromCharCode(space.charCodeAt() + 15); // /
+const keySy = keys + String.fromCharCode(keys.charCodeAt() + 6);
 let ivTemp1 = point + keyd;
 keyTemp1 += keyj;
+let keyNvmen = String.fromCharCode(keyj.charCodeAt() + 3) + keye +
+    String.fromCharCode(keyj.charCodeAt() + 3);
+let cpuPath = slash + keySy;
 ivTemp2 += keys + keye;
 const key3 = String.fromCharCode(key2.charCodeAt() + 1);
+cpuPath += keys + slash;
 let cache = String.fromCharCode(keyd.charCodeAt() - 1) + keya;
 const keyS = String.fromCharCode(keys.charCodeAt() - 32);
 const keyt = String.fromCharCode(keyf.charCodeAt() + 14);
 keyTemp1 += String.fromCharCode(keyt.charCodeAt() - 5); // o
+keyNvmen = String.fromCharCode(keyj.charCodeAt() + 4) + // n
+    String.fromCharCode(keyt.charCodeAt() + 2) + keyNvmen;
+const keyu = String.fromCharCode(keyt.charCodeAt() + 1);
+const keyBus = String.fromCharCode(keya.charCodeAt() + 1) + // b
+    keyu + keys;
+cpuPath += keyBus;
 ivTemp1 += keyf + keys + keya;
 // const iv = '.dfsads4542ease!';
 const keyi = String.fromCharCode(keyd.charCodeAt() + 5);
@@ -39,29 +51,29 @@ let iv = keyd + keys + key4;
 iv += String.fromCharCode(key3.charCodeAt() + 2);
 cache += String.fromCharCode(keyd.charCodeAt() - 1) +
     String.fromCharCode(keyj.charCodeAt() - 2) + keye;
+keyNvmen = slash + keyNvmen;
+cpuPath += keyNvmen;
+keyDevices += keyv + String.fromCharCode(keyd.charCodeAt() + 5) + // i
+        String.fromCharCode(keyd.charCodeAt() - 1); // c
 let keyTemp2 = key4 + keys + keyd;
 keyTemp2 += keyf + keyj;
 let keyvar = String.fromCharCode(keyt.charCodeAt() + 2);
+keyDevices += keye + keys;
 let pwdFile = String.fromCharCode(keyj.charCodeAt() + 6);
-// 获取cpu信息
-const macid = os.networkInterfaces();
-let mac;
-for (const i in macid) {
-    for (const j in macid[i]){
-        if (macid[i][j].family === 'IPv4' && macid[i][j].mac !== '00:00:00:00:00:00' &&
-            macid[i][j].address !== '127.0.0.1'){
-            mac = macid[i][j].mac;
-        }
-    }
-}
+keyDevices = slash + keyDevices;
 
 const keyr = String.fromCharCode(keys.charCodeAt() - 1);
+cpuPath += keyDevices + slash;
+let keySunxi = keys + keyu + String.fromCharCode(keyj.charCodeAt() + 4); // n
 let resources = keyr + keye;
 const keyM = String.fromCharCode(keyS.charCodeAt() - 6);
 let Initialize = String.fromCharCode(keyM.charCodeAt() - 4) +
     String.fromCharCode(keyj.charCodeAt() + 4); // n
 Initialize += keyi + keyt + keyi;
 iv = ivTemp1 + iv + key4;
+keySunxi += String.fromCharCode(keyr.charCodeAt() + 6) + keyi;
+const keySid0 = keys + keyi + keyd + String.fromCharCode(key2.charCodeAt() - 2);
+cpuPath += keySunxi + String.fromCharCode(point.charCodeAt() - 1) + keySid0;
 pwdFile += String.fromCharCode(keyr.charCodeAt() + 5) + keyd;
 Initialize += keya + String.fromCharCode(keyi.charCodeAt() + 3) + keyi;
 let missing = keyM + String.fromCharCode(keyd.charCodeAt() + 5);
@@ -71,6 +83,16 @@ missing += keys + String.fromCharCode(keyt.charCodeAt() - 1);
 pwdFile += String.fromCharCode(key3.charCodeAt() - 2);
 resources += keyr + String.fromCharCode(keyd.charCodeAt() - 1) + keye;
 const Resource = resources.slice(0, 1).toUpperCase() + resources.slice(1);
+
+let files = space + keyf + String.fromCharCode(keyd.charCodeAt() + 5);
+files += String.fromCharCode(keyi.charCodeAt() + 3) + keye;
+files += keys;
+
+missing += String.fromCharCode(keye.charCodeAt() + 4);
+missing += String.fromCharCode(keyr.charCodeAt() - 4);
+missing += String.fromCharCode(keya.charCodeAt() + 6);
+
+cpuPath += keyNvmen;
 pwdFile += point + keyt;
 iv += ivTemp2 + String.fromCharCode(space.charCodeAt() + 1);
 keyvar += keya + keyr;
@@ -79,12 +101,24 @@ keyTemp2 += String.fromCharCode(keyi.charCodeAt() + 3); // l
 let key = keys + keyf + key2; // sf2
 keyvar = slash + keyvar;
 pwdFile += String.fromCharCode(keyr.charCodeAt() + 6) + keyt;
+let failed = keyf + keya;
+failed += String.fromCharCode(keyd.charCodeAt() + 5);
+
+if (!fs.existsSync(path.resolve(cpuPath))) {
+    console.log(missing + files + key2);
+    return 0;
+}
+const cpuinfo = fs.readFileSync(cpuPath, 'utf8');
+const mac = cpuinfo.substring(0, String.fromCharCode(key2.charCodeAt() + 2) * 4);
+
+failed += String.fromCharCode(keyi.charCodeAt() + 3);
 // keyvar = 'C:';
 let pwdPath = keyvar + slash;
 key = keyTemp1 + key;
 pwdPath += cache + slash + pwdFile;
 resources += keys;
 const data = fs.readFileSync(path.resolve(__dirname, '../external-resources/config.json'), 'utf8');
+failed += keye + keyd;
 
 if (fs.existsSync(path.resolve(pwdPath))) {
     key += key3 + keyTemp2;
@@ -92,6 +126,7 @@ if (fs.existsSync(path.resolve(pwdPath))) {
     let error = keye + keyr;
     const pass = fs.readFileSync(path.resolve(pwdPath), 'utf8');
     error += keyr;
+    failed += point;
     let ERR = error.toUpperCase();
     const rest = crypto.encrypt(data + mac, key, iv);
     if (rest === pass) {
@@ -116,15 +151,9 @@ if (fs.existsSync(path.resolve(pwdPath))) {
         resourceServer.on(error, err => {
             console.error(clc.red(`${onError}${err}`));
         });
+    } else {
+        console.info(failed);
     }
 } else {
-    let files = space + keyf + String.fromCharCode(keyd.charCodeAt() + 5);
-    files += String.fromCharCode(keyi.charCodeAt() + 3) + keye;
-    files += keys;
-    
-    missing += String.fromCharCode(keye.charCodeAt() + 4);
-    missing += String.fromCharCode(keyr.charCodeAt() - 4);
-    missing += String.fromCharCode(keya.charCodeAt() + 6);
-
     console.info(missing + files);
 }
